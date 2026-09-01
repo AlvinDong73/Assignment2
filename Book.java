8758fd616527613b72a91cc486dba524bbc3c7a7
Alvin Dong <alvin.dong@drake.edu>
2026-09-01 11:46:54 -0500

    initialize 2 book objects from input

--- Book.java
+++ Book.java
@@ -17,7 +17,29 @@ public class Book {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        Book book1 = new Book("test", "me", 2000);
+        System.out.println("Currently creating 2 books!");
+        
+        System.out.println("What is the name of the first book?");
+        String book1Name = keyboard.nextLine();
+        System.out.println("Who is the author of the first book?");
+        String book1Author = keyboard.nextLine();
+        System.out.println("When was the first book written?");
+        int book1Year = keyboard.nextInt();
+
+        Book book1 = new Book(book1Name, book1Author, book1Year);
+
+        System.out.println("What is the name of the second book?");
+        String book2Name = keyboard.nextLine();
+        System.out.println("Who is the author of the second book?");
+        String book2Author = keyboard.nextLine();
+        System.out.println("When was the second book written?");
+        int book2Year = keyboard.nextInt();
+
+        Book book2 = new Book(book2Name, book2Author, book2Year);
+
         book1.display();
+        book2.display();
+
+        keyboard.close();
     }
 }
