18fb187c392b357ec94e5d7724ee2e625099e2b0
Alvin Dong <alvin.dong@drake.edu>
2026-09-01 11:58:27 -0500

    move code into main file

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Currently creating 2 books!");
+        
+        System.out.println("What is the name of the first book?");
+        String book1Name = keyboard.nextLine();
+        System.out.println("Who is the author of the first book?");
+        String book1Author = keyboard.nextLine();
+        System.out.println("When was the first book written?");
+        int book1Year = keyboard.nextInt();
+        keyboard.nextLine();
+
+        Book book1 = new Book(book1Name, book1Author, book1Year);
+        
+        System.out.println("What is the name of the second book?");
+        String book2Name = keyboard.nextLine();
+        System.out.println("Who is the author of the second book?");
+        String book2Author = keyboard.nextLine();
+        System.out.println("When was the second book written?");
+        int book2Year = keyboard.nextInt();
+
+        Book book2 = new Book(book2Name, book2Author, book2Year);
+
+        book1.display();
+        book2.display();
+
+        keyboard.close();
+    }
+}
